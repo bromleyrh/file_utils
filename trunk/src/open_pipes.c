b37767962b9705f5acdc276053e33791e62ea026
Richard Bromley <bromleyrh@gmail.com>
2015-12-19 22:50:28 -0800

    Closed unneeded file descriptors in open_pipes

--- trunk/src/open_pipes.c
+++ trunk/src/open_pipes.c
@@ -28,7 +28,7 @@ open_pipes(int pipe1[2], int pipe2[2])
 {
     if ((pipe(pipe1) == -1) || (pipe(pipe2) == -1)) {
         error(0, errno, "Error opening pipe");
-        return -1;
+        goto err;
     }
 
     if ((dup2(pipe1[0], BASE_FD) == -1)
@@ -36,10 +36,22 @@ open_pipes(int pipe1[2], int pipe2[2])
         || (dup2(pipe2[0], BASE_FD + 2) == -1)
         || (dup2(pipe2[1], BASE_FD + 3) == -1)) {
         error(0, errno, "Error duplicating file descriptor");
-        return -1;
+        goto err;
     }
 
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+
     return 0;
+
+err:
+    close(pipe1[0]);
+    close(pipe1[1]);
+    close(pipe2[0]);
+    close(pipe2[1]);
+    return -1;
 }
 
 int
@@ -51,16 +63,11 @@ main(int argc, char **argv)
         return EXIT_FAILURE;
 
     if (open_pipes(pipe1, pipe2) == -1)
-        goto err;
+        return EXIT_FAILURE;
 
     execvp(cmd[0], cmd);
     error(0, errno, "Error executing %s", cmd[0]);
 
-err:
-    close(pipe1[0]);
-    close(pipe1[1]);
-    close(pipe2[0]);
-    close(pipe2[1]);
     return EXIT_FAILURE;
 }
 
