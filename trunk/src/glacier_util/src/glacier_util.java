db3bcbc3018eeab25141022ba666f0e00cd351fb
Richard Bromley <bromleyrh@gmail.com>
2015-12-24 23:25:53 -0800

    Committed initial work in glacier_util branch (3)

--- trunk/src/glacier_util/src/glacier_util.java
+++ trunk/src/glacier_util/src/glacier_util.java
@@ -5,56 +5,121 @@
 import java.io.*;
 import java.nio.file.*;
 
-class inventory {
-    private final Path pathname;
-    private BufferedReader f;
-
-    public inventory(String _pathname)
+final class parser {
+    private int indent;
+    
+    public parser()
     {
-        pathname = Paths.get(_pathname);
+        this.reset();
+    }
+
+    public String parse(String s)
+    {
+        int i, j;
+        StringBuilder ret = new StringBuilder();
+
+        for (i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            switch (c) {
+            case '{':
+            case '[':
+                ++indent;
+                ret.append(Character.toString(c));
+                ret.append("\n");
+                for (j = 0; j < indent; j++)
+                    ret.append("\t");
+                break;
+            case '}':
+            case ']':
+                --indent;
+                ret.append("\n");
+                for (j = 0; j < indent; j++)
+                    ret.append("\t");
+                ret.append(Character.toString(c));
+                break;
+            case ',':
+                ret.append(",\n");
+                for (j = 0; j < indent; j++)
+                    ret.append("\t");
+                break;
+            default:
+                ret.append(Character.toString(c));
+            }
+        }
+
+        return ret.toString();
+    }
+    
+    public void reset()
+    {
+        indent = 0;
+    }
+}
+
+final class inventory {
+    private final String pathname;
+    private final BufferedReader f;
+
+    public inventory(String _pathname) throws IOException
+    {
+        InputStream is;
+
+        pathname = _pathname;
+        is = Files.newInputStream(Paths.get(pathname));
+        f = new BufferedReader(new InputStreamReader(is));
     }
     
     public String getPath()
     {
-        return pathname.toString();
+        return pathname;
     }
 
-    public void open() throws IOException
+    public void parse() throws IOException
     {
-        InputStream is = Files.newInputStream(pathname);
-        f = new BufferedReader(new InputStreamReader(is));
-    }
+        char[] buf = new char[1024 * 1024];
+        parser p = new parser();
 
-    public boolean parse()
-    {
-        if (f == null)
-            return false;
-    
-        System.out.format("%s\n", pathname.toString());
-        return true;
+        for (;;) {
+            boolean end = false;
+            int off, numread;
+
+            for (off = 0; off < buf.length; off += numread) {
+                numread = f.read(buf, off, buf.length - off);
+                if (numread == -1) {
+                    end = true;
+                    break;
+                }
+            }
+
+            System.out.print(p.parse(new String(buf, 0, off)));
+            if (end)
+                break;
+        }
+
+        System.out.print("\n");
     }
 }
 
 public final class glacier_util {
     public static void main(String[] args)
     {
+        inventory i;
         String file;
 
         if (args.length == 0) {
-            System.err.format("Must specify file\n");
+            System.err.print("Must specify file\n");
             System.exit(1);
         }
         file = args[0];
 
-        inventory i = new inventory(file);
         try {
-            i.open();
+            i = new inventory(file);
+            i.parse();
         } catch (IOException e) {
-            System.err.format("Couldn't open inventory %s", i.getPath());
+            System.err.format("Couldn't parse inventory %s", file);
             System.exit(1);
         }
-        if (i.parse() == false)
-            System.exit(1);
 
         System.exit(0);
     }
