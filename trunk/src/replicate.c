84e526987ae7ef4e3a99eacdcc21621c47a0d189
Richard Bromley <bromleyrh@gmail.com>
2017-01-01 17:57:10 -0800

    Fixed replicate/verify to unmount unneeded FS's

--- trunk/src/replicate.c
+++ trunk/src/replicate.c
@@ -106,6 +106,12 @@ static void int_handler(int);
 
 static int set_signal_handlers(void);
 
+static int mnt_cmp(struct libmnt_table *, struct libmnt_fs *,
+                   struct libmnt_fs *);
+static int keep_mnt(const char *);
+
+static int unshare_mnt_ns(void);
+
 static int mount_filesystem(const char *, const char *, int);
 static int unmount_filesystem(const char *, int);
 
@@ -649,6 +655,133 @@ set_signal_handlers()
 }
 
 static int
+mnt_cmp(struct libmnt_table *tbl, struct libmnt_fs *fs1, struct libmnt_fs *fs2)
+{
+    const char *target1, *target2;
+
+    (void)tbl;
+
+    if (((target1 = mnt_fs_get_target(fs1)) == NULL)
+        || ((target2 = mnt_fs_get_target(fs2)) == NULL))
+        return -1;
+
+    return strcmp(target1, target2);
+}
+
+#define MNT(target) {target, sizeof(target) - 1}
+
+static int
+keep_mnt(const char *target)
+{
+    size_t i;
+
+    static const struct {
+        const char  *target;
+        size_t      len;
+    } keep[] = {
+        MNT("/dev"),
+        MNT("/proc"),
+        MNT("/run"),
+        MNT("/sys"),
+        MNT("/tmp")
+    }, *mnt;
+
+    if (strcmp("/", target) == 0)
+        return 1;
+
+    for (i = 0; i < sizeof(keep)/sizeof(keep[0]); i++) {
+        mnt = &keep[i];
+
+        if ((strncmp(target, mnt->target, mnt->len) == 0)
+            && ((target[mnt->len] == '\0') || (target[mnt->len] == '/')))
+            return 1;
+    }
+
+    return 0;
+}
+
+#undef MNT
+
+static int
+unshare_mnt_ns()
+{
+    int ret;
+    struct libmnt_context *mntctx;
+    struct libmnt_fs *fs;
+    struct libmnt_iter *itr;
+    struct libmnt_table *tbl;
+
+    /* requires CAP_SYS_ADMIN */
+    if (unshare(CLONE_NEWNS) == -1) {
+        error(0, errno, "Error unsharing namespace");
+        return -errno;
+    }
+
+    tbl = mnt_new_table();
+    if (tbl == NULL)
+        return -ENOMEM;
+    itr = mnt_new_iter(MNT_ITER_FORWARD);
+    if (itr == NULL) {
+        mnt_free_table(tbl);
+        return -ENOMEM;
+    }
+    mntctx = mnt_new_context();
+    if (mntctx == NULL) {
+        mnt_free_table(tbl);
+        mnt_free_iter(itr);
+        return -ENOMEM;
+    }
+    if (mnt_context_disable_mtab(mntctx, 1) != 0)
+        goto err;
+
+    if (mnt_table_parse_mtab(tbl, NULL) != 0)
+        goto err;
+
+    if (mnt_table_uniq_fs(tbl, 0, &mnt_cmp) != 0)
+        goto err;
+
+    if ((mnt_table_first_fs(tbl, &fs) != 0)
+        || (mnt_table_set_iter(tbl, itr, fs) != 0))
+        goto err;
+
+    for (;;) {
+        const char *target;
+
+        target = mnt_fs_get_target(fs);
+        if (target == NULL)
+            goto err;
+
+        if (!keep_mnt(target)) {
+            debug_print("Unmounting %s in private namespace", target);
+            if ((mnt_reset_context(mntctx) != 0)
+                || (mnt_context_set_fs(mntctx, fs) != 0)
+                || (mnt_context_umount(mntctx) != 0))
+                goto err;
+        }
+
+        ret = mnt_table_next_fs(tbl, itr, &fs);
+        if (ret != 0) {
+            if (ret == 1)
+                break;
+            goto err;
+        }
+    }
+
+    mnt_free_context(mntctx);
+    mnt_free_iter(itr);
+    mnt_free_table(tbl);
+
+    return 0;
+
+err:
+    error(0, 0, "Error parsing /etc/mtab");
+    mnt_free_context(mntctx);
+    mnt_free_iter(itr);
+    mnt_free_table(tbl);
+    return -EIO;
+}
+
+static int
 mount_filesystem(const char *devpath, const char *mntpath, int read)
 {
     int mflags;
@@ -1009,12 +1142,9 @@ main(int argc, char **argv)
     if (log)
         openlog(NULL, LOG_PID, LOG_USER);
 
-    /* requires CAP_SYS_ADMIN */
-    if (unshare(CLONE_NEWNS) == -1) {
-        error(0, errno, "Error unsharing namespace");
-        ret = -errno;
+    ret = unshare_mnt_ns();
+    if (ret != 0)
         goto end;
-    }
 
     ret = set_signal_handlers();
     if (ret != 0)
