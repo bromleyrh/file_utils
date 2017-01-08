320803cc213930fbed733bb8f857b7d5474642fd
Richard Bromley <bromleyrh@gmail.com>
2017-01-08 15:09:01 -0800

    Expanded mkmanifest to write header automatically

--- trunk/src/mkmanifest.c
+++ trunk/src/mkmanifest.c
@@ -3,16 +3,46 @@
  */
 
 #include <proc_ext.h>
+#include <strings_ext.h>
 
 #include <errno.h>
 #include <error.h>
+#include <limits.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
+#include <wordexp.h>
 
 #include <sys/wait.h>
 
+#define TEMPLATE_PATH "\"$HOME/.manifest_temp\""
+
+static int get_template_path(const char *, char *);
+
 static int set_signal_handlers(void);
+
+static int
+get_template_path(const char *pathspec, char *path)
+{
+    wordexp_t words;
+
+    if (wordexp(pathspec, &words, WRDE_NOCMD | WRDE_UNDEF) != 0)
+        goto err1;
+
+    if (words.we_wordc != 1)
+        goto err2;
+
+    strlcpy(path, words.we_wordv[0], PATH_MAX);
+
+    wordfree(&words);
+
+    return 0;
+
+err2:
+    wordfree(&words);
+err1:
+    return -EIO;
+}
 
 static int
 set_signal_handlers()
@@ -39,17 +69,24 @@ main(int argc, char **argv)
 {
     int err;
 
+    static char template_path[PATH_MAX];
+
     static char *const cmd1[] = {"verify", NULL};
     static char *const cmd2[] = {"osort", "-k3", NULL};
+    static char *const cmd3[] = {"cat", template_path, "-", NULL};
 
     struct proc cmds[] = {
         {.file = "verify", .argv = cmd1},
-        {.file = "osort", .argv = cmd2}
+        {.file = "osort", .argv = cmd2},
+        {.file = "cat", .argv = cmd3}
     };
     static const int ncmds = (int)(sizeof(cmds)/sizeof(cmds[0]));
 
     (void)argc;
     (void)argv;
+
+    if (get_template_path(TEMPLATE_PATH, template_path) != 0)
+        return EXIT_FAILURE;
 
     if (set_signal_handlers() != 0)
         return EXIT_FAILURE;
