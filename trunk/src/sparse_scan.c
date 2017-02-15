356c0732f27ae987af5f3ad2592092a2d17bf180
Richard Bromley <bromleyrh@gmail.com>
2017-02-14 19:46:45 -0800

    Committed initial work in sparse_scan (2)

--- trunk/src/sparse_scan.c
+++ trunk/src/sparse_scan.c
@@ -2,44 +2,54 @@
  * sparse_scan.c
  */
 
+#define _FILE_OFFSET_BITS 64
+
 #include <strings_ext.h>
+
+#include <files/util.h>
 
 #include <errno.h>
 #include <fcntl.h>
+#include <inttypes.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include <sys/param.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#define BLKSIZE 512
+#define BLKSIZE 4096
 
-static void print_block_range(int, int);
-static void scan_data(const char *, size_t, size_t, int *);
+static void print_block_range(int64_t, int64_t);
+static void scan_data(const char *, size_t, off_t, int *);
 
 static int do_zero_block_scan(int);
 
+static int block_scan_cb(int, int, const char *, const char *, struct stat *,
+                         void *);
+
 static void
-print_block_range(int begin, int end)
+print_block_range(int64_t begin, int64_t end)
 {
-    int numblk, start;
+    int64_t numblk, start;
 
     start = begin + 1;
     numblk = end - start;
 
     if (numblk > 1)
-        printf("%d - %d\n", start, end - 1);
+        printf("Blocks %" PRIi64 " - %" PRIi64 "\n", start, end - 1);
     else if (numblk == 1)
-        printf("%d\n", start);
+        printf("Block %" PRIi64 "\n", start);
 }
 
 static void
-scan_data(const char *buf, size_t len, size_t off, int *lastnonzero)
+scan_data(const char *buf, size_t len, off_t off, int *lastnonzero)
 {
-    int nextblk;
     int nonzero1, nonzero2;
+    int64_t nextblk;
     size_t cmplen1, cmplen2;
 
     nextblk = (off + BLKSIZE) / BLKSIZE;
@@ -88,12 +98,31 @@ do_zero_block_scan(int fd)
     return 0;
 }
 
+static int
+block_scan_cb(int fd, int dirfd, const char *name, const char *path,
+              struct stat *s, void *ctx)
+{
+    (void)dirfd;
+    (void)name;
+    (void)ctx;
+
+    if (!S_ISREG(s->st_mode))
+        return 0;
+
+    puts(path);
+
+    return do_zero_block_scan(fd);
+}
+
 int
 main(int argc, char **argv)
 {
     const char *path;
     int err;
     int fd;
+    struct stat s;
+
+    setlinebuf(stdout);
 
     if (argc < 2) {
         fprintf(stderr, "Must specify file\n");
@@ -103,11 +132,21 @@ main(int argc, char **argv)
 
     fd = open(path, O_NOCTTY | O_RDONLY);
     if (fd == -1) {
-        fprintf(stderr, "Error opening %s\n", path);
+        fprintf(stderr, "Error opening %s: %s\n", path, strerror(errno));
         return EXIT_FAILURE;
     }
 
-    err = do_zero_block_scan(fd);
+    if (fstat(fd, &s) == -1) {
+        fprintf(stderr, "Error getting stats of %s: %s\n", path,
+                strerror(errno));
+        close(fd);
+        return EXIT_FAILURE;
+    }
+
+    if (S_ISDIR(s.st_mode))
+        err = dir_walk_fd(fd, &block_scan_cb, DIR_WALK_ALLOW_ERR, NULL);
+    else
+        err = do_zero_block_scan(fd);
 
     close(fd);
 
