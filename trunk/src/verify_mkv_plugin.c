492562ba945945f3d04a39a51bd117a4b8deea18
Richard Bromley <bromleyrh@gmail.com>
2023-11-25 00:28:09 -0800

    Update .mkv plugin to use new library interface
    Commit UUID: 2eb611d0-52d7-45ae-b04f-f0ef1658ce6c

--- trunk/src/verify_mkv_plugin.c
+++ trunk/src/verify_mkv_plugin.c
@@ -153,19 +153,21 @@ metadata_cb(const char *id, matroska_metadata_t *val, size_t len, size_t hdrlen,
 }
 
 static int
-bitstream_cb(uint64_t trackno, const void *buf, size_t len, size_t totlen,
-             size_t hdrlen, size_t num_logical_bytes, off_t off, int16_t ts,
-             int keyframe, void *ctx)
+bitstream_cb(uint64_t trackno, const void *buf, size_t len, size_t framelen,
+             size_t totlen, size_t hdrlen, size_t num_logical_bytes, off_t off,
+             int16_t ts, int new_frame, int keyframe, void *ctx)
 {
     int res;
     struct mkv_ctx *mctx = ctx;
 
     (void)buf;
     (void)len;
+    (void)framelen;
     (void)totlen;
     (void)hdrlen;
     (void)num_logical_bytes;
     (void)off;
+    (void)new_frame;
     (void)keyframe;
 
     if (mctx->state == 4) {
