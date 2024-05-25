db8c25ec37a4702433cd46232e255e5194b89975
Richard Bromley <bromleyrh@gmail.com>
2024-05-24 21:41:12 -0700

    Update replicate to use new libjson interfaces
    Commit UUID: 1ef49d6e-c1a5-4025-b944-704a1d65ca44

--- trunk/src/replicate_conf.c
+++ trunk/src/replicate_conf.c
@@ -278,8 +278,8 @@ read_copy_creds_opt(json_value_t opt, void *data)
 
     err = json_object_get(opt, L"uid", &elem);
     if (!err) {
-        str = json_string_get_value(elem.value);
-        json_value_put(elem.value);
+        str = json_string_get_value(elem.v);
+        json_value_put(elem.v);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
@@ -292,8 +292,8 @@ read_copy_creds_opt(json_value_t opt, void *data)
         err = json_object_get(opt, L"gid", &elem);
         if (err)
             return ERR_TAG(-err);
-        str = json_string_get_value(elem.value);
-        json_value_put(elem.value);
+        str = json_string_get_value(elem.v);
+        json_value_put(elem.v);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         omemset(&s, 0);
@@ -307,8 +307,8 @@ read_copy_creds_opt(json_value_t opt, void *data)
         err = json_object_get(opt, L"user", &elem);
         if (err)
             return ERR_TAG(-err);
-        str = json_string_get_value(elem.value);
-        json_value_put(elem.value);
+        str = json_string_get_value(elem.v);
+        json_value_put(elem.v);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
@@ -323,8 +323,8 @@ read_copy_creds_opt(json_value_t opt, void *data)
         err = json_object_get(opt, L"group", &elem);
         if (err)
             return ERR_TAG(-err);
-        str = json_string_get_value(elem.value);
-        json_value_put(elem.value);
+        str = json_string_get_value(elem.v);
+        json_value_put(elem.v);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         omemset(&s, 0);
@@ -465,14 +465,14 @@ read_json_config(json_value_t config, struct replicate_ctx *ctx)
         if (err)
             return ERR_TAG(-err);
 
-        opt = &opts[hash_wcs(elem.key, -1) >> 6 & 7];
-        if (opt->opt == NULL || wcscmp(elem.key, opt->opt) != 0) {
-            json_value_put(elem.value);
+        opt = &opts[hash_wcs(elem.k, -1) >> 6 & 7];
+        if (opt->opt == NULL || wcscmp(elem.k, opt->opt) != 0) {
+            json_value_put(elem.v);
             return ERR_TAG(EIO);
         }
 
-        err = (*opt->fn)(elem.value, ctx);
-        json_value_put(elem.value);
+        err = (*opt->fn)(elem.v, ctx);
+        json_value_put(elem.v);
         if (err)
             return err;
     }
