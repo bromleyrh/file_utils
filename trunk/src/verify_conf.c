fa5e9792b08c63ae4a4c1d6d5578256242515b4f
Richard Bromley <bromleyrh@gmail.com>
2024-05-24 20:29:36 -0700

    Fix resource leaks in read_creds_opt()
    Commit UUID: 9290d895-dc46-4bbc-98bf-4b1347713fdc

--- trunk/src/verify_conf.c
+++ trunk/src/verify_conf.c
@@ -292,6 +292,7 @@ read_creds_opt(json_val_t opt, void *data)
     err = json_val_object_get_elem_by_key(opt, L"uid", &elem);
     if (!err) {
         str = json_val_string_get(elem.value);
+        json_val_free(elem.value);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
@@ -305,6 +306,7 @@ read_creds_opt(json_val_t opt, void *data)
         if (err)
             return ERR_TAG(-err);
         str = json_val_string_get(elem.value);
+        json_val_free(elem.value);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         omemset(&s, 0);
@@ -319,6 +321,7 @@ read_creds_opt(json_val_t opt, void *data)
         if (err)
             return ERR_TAG(-err);
         str = json_val_string_get(elem.value);
+        json_val_free(elem.value);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
@@ -334,6 +337,7 @@ read_creds_opt(json_val_t opt, void *data)
         if (err)
             return ERR_TAG(-err);
         str = json_val_string_get(elem.value);
+        json_val_free(elem.value);
         if (str == NULL)
             return ERR_TAG(ENOMEM);
         omemset(&s, 0);
