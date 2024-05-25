15f57044b4134af4c652297832a098bc7b240b6b
Richard Bromley <bromleyrh@gmail.com>
2024-05-24 20:33:44 -0700

    Fix memory leaks in read_creds_opt()
    Commit UUID: 9b52ec98-b236-47de-b92a-8448f9484dc8

--- trunk/src/verify_conf.c
+++ trunk/src/verify_conf.c
@@ -285,30 +285,46 @@ read_creds_opt(json_val_t opt, void *data)
     json_object_elem_t elem;
     mbstate_t s;
     struct verify_ctx *ctx = data;
+    wchar_t *str;
 
     omemset(&s, 0);
 
     err = json_val_object_get_elem_by_key(opt, L"uid", &elem);
     if (!err) {
-        if (awcstombs(&buf, json_val_string_get(elem.value), &s) == (size_t)-1)
-            return -errno;
+        str = json_val_string_get(elem.value);
+        if (str == NULL)
+            return ERR_TAG(ENOMEM);
+        err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
+        free(str);
+        if (err)
+            return err;
         ctx->uid = atoi(buf);
         free(buf);
 
         err = json_val_object_get_elem_by_key(opt, L"gid", &elem);
         if (err)
             return ERR_TAG(-err);
+        str = json_val_string_get(elem.value);
+        if (str == NULL)
+            return ERR_TAG(ENOMEM);
         omemset(&s, 0);
-        if (awcstombs(&buf, json_val_string_get(elem.value), &s) == (size_t)-1)
-            return -errno;
+        err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
+        free(str);
+        if (err)
+            return err;
         ctx->gid = atoi(buf);
         free(buf);
     } else if (err == -EINVAL) {
-        err = json_val_object_get_elem_by_key(opt, L"user", &elem);
+        err = json_object_get(opt, L"user", &elm);
+        if (err)
+            return ERR_TAG(-err);
+        str = json_val_string_get(elem.value);
+        if (str == NULL)
+            return ERR_TAG(ENOMEM);
+        err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
+        free(str);
         if (err)
             return err;
-        if (awcstombs(&buf, json_val_string_get(elem.value), &s) == (size_t)-1)
-            return -errno;
         err = get_uid(buf, &ctx->uid);
         free(buf);
         if (err)
@@ -317,9 +333,14 @@ read_creds_opt(json_val_t opt, void *data)
         err = json_val_object_get_elem_by_key(opt, L"group", &elem);
         if (err)
             return ERR_TAG(-err);
+        str = json_val_string_get(elem.value);
+        if (str == NULL)
+            return ERR_TAG(ENOMEM);
         omemset(&s, 0);
-        if (awcstombs(&buf, json_val_string_get(elem.value), &s) == (size_t)-1)
-            return -errno;
+        err = awcstombs(&buf, str, &s) == (size_t)-1 ? ERR_TAG(errno) : 0;
+        free(str);
+        if (err)
+            return err;
         if (strcmp(buf, "-") == 0)
             ctx->gid = (gid_t)-1;
         else {
