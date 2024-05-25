e6e3ed53e35aaeb4ae5874622fa7043de3981120
Richard Bromley <bromleyrh@gmail.com>
2024-05-24 20:34:56 -0700

    Fix memory leak in read_input_file_opt()
    Commit UUID: 9d8d8ab3-5037-4768-b133-1b45b0f072e8

--- trunk/src/verify_conf.c
+++ trunk/src/verify_conf.c
@@ -431,12 +431,20 @@ read_exclude_opt(json_val_t opt, void *data)
 static int
 read_input_file_opt(json_val_t opt, void *data)
 {
+    int err;
     mbstate_t s;
     struct verify_ctx *ctx = data;
+    wchar_t *str;
+
+    str = json_val_string_get(opt);
+    if (str == NULL)
+        return ERR_TAG(ENOMEM);
 
     omemset(&s, 0);
-    return awcstombs(&ctx->input_file, json_val_string_get(opt), &s)
-           == (size_t)-1 ? ERR_TAG(errno) : 0;
+    err = awcstombs(&ctx->input_file, str, &s) == (size_t)-1
+          ? ERR_TAG(errno) : 0;
+    free(str);
+    return err;
 }
 
 static int
